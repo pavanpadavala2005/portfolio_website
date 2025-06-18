3c0c9c6ad5b89c6bdc7c1cf14489b6d769f24dde
pavanpadavala2005 <rokzpavan143@gmail.com>
2025-06-18 08:44:14 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./inde
 import App from "./App.jsx";
 import { store } from "./app/app.js";
 import { Provider } from "react-redux";
